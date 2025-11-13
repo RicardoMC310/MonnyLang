95869c5acc75134e3f4d05dc4c812fda683b2752
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2025-11-12 21:39:11 -0300

    reiniciando com C e C++

--- include/utils/System.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-class System
-{
-public:
-    static void clear();
-};
--- xmake.lua
+++
@@ -1,7 +0,0 @@
-add_rules("mode.debug", "mode.release")
-
-target("MonnyLang")
-    set_kind("binary")
-    add_files("src/**.cpp")
-    add_includedirs("./include")
-    set_optimize("fastest")
